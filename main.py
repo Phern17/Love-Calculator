d07a38c0c9f01b2e2e206120cf640088ffa28220
Phern17 <mphern17@gmail.com>
2020-12-30 15:40:08 +0000

    Optimal solution

--- main.py
+++ main.py
@@ -5,36 +5,27 @@ name2 = input("What is their name? \n").lower()
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-num_T = name1.count('t') + name2.count('t')
-num_R = name1.count('r') + name2.count('r')
-num_U = name1.count('u') + name2.count('u')
-num_E = name1.count('e') + name2.count('e')
+combine_name = name1 + name2
+lower_name = combine_name.lower()
 
-num_L = name1.count('l') + name2.count('l')
-num_O = name1.count('o') + name2.count('o')
-num_V = name1.count('v') + name2.count('v')
-num_E = name1.count('e') + name2.count('e')
+T = lower_name.count('t')
+R = lower_name.count('r')
+U = lower_name.count('u')
+E = lower_name.count('e')
 
-print(f"T occurs {num_T} times.")
-print(f"R occurs {num_R} times.")
-print(f"U occurs {num_U} times.")
-print(f"E occurs {num_E} times.\n")
+L = lower_name.count('l')
+O = lower_name.count('o')
+V = lower_name.count('v')
 
-print(f"L occurs {num_L} times.")
-print(f"O occurs {num_O} times.")
-print(f"V occurs {num_V} times.")
-print(f"E occurs {num_E} times.")
+front_num = T + R + U + E
+back_num = L + O + V + E
 
-front_num = num_T + num_R + num_U + num_E
-back_num = num_L + num_O + num_V + num_E
+final_score = int(str(front_num) + str(back_num))
 
-string_num = str(front_num) + str(back_num)
-final_score = int(string_num)
-
-if (final_score > 40 and final_score < 50):
+if (final_score >= 40) and (final_score <= 50):
   print(f"Your score is: {final_score}, you are alright together.")
 
-elif (final_score < 10 or final_score > 90):
+elif (final_score < 10) or (final_score > 90):
   print(f"Your score is: {final_score}, you go together like coke and mentos.")
 
 else:
